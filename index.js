6bc2adbb1c3dd9af112c3c6fda03281608a85202
Mika Äärilä <maarila@gmail.com>
2018-01-05 12:09:14 +0200

    tehtävä 55

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get("/api/persons", (req, res) => {
     })
     .catch((error) => {
       console.log(error);
+      res.status(404).end();
     });
 });
 
@@ -46,9 +47,13 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      res.status(400).send({error: "malformatted id"});
+    });
 });
 
 app.post("/api/persons", (req, res) => {
